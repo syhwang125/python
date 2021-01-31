761c3f48a2b7cec6a04a40006daa68b3746700f0
syhwang125 <syhwang125@poscoict.com>
2021-01-31 16:30:56 +0900

    elice online pratice

---
+++ elice7-모듈.py
@@ -0,0 +1,43 @@
+# 2. time 모듈을 임포트하세요
+import time
+
+# 아래는 출력을 위한 코드입니다. 수정하지 마세요.
+print('5초간 프로그램을 정지합니다.')
+
+# time 모듈의 sleep() 함수를 사용해 프로그램을 5초 정지합니다.
+time.sleep(5)
+
+# 아래는 출력을 위한 코드입니다. 수정하지 마세요.
+print('5초가 지났습니다.')
+
+# 1. 엘리스가 작성한 mylib 모듈을 임포트하세요.
+import mylib
+
+# 2. 파이썬 내장 모듈 time을 임포트하세요.
+import time
+
+# 3. 파이썬 내장 모듈 math을 임포트하세요.
+import math
+
+# 아래는 출력을 위한 코드입니다. 수정하지 마세요.
+print(mylib.add_txt('나는', '파이썬이다'))
+time.sleep(2)
+print(math.floor(3.3))
+
+# 1. time 모듈의 sleep 함수를 임포트하세요.
+from time import sleep
+
+# 2. time 모듈의 sleep 함수를 호출해보세요.
+sleep(2)
+
+# 3. mylib 모듈의 reverse 함수를 임포트하세요.
+from mylib import reverse
+
+# 4. mylib 모듈의 reverse 함수를 호출해보세요.
+print(reverse(1, 2, 3))
+
+# 5. math 모듈의 ceil 함수를 임포트하세요.
+from math import ceil
+
+# 6. math 모듈의 ceil 함수를 호출해보세요.
+print(ceil(2.4))
